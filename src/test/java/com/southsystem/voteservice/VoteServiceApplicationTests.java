9051d9fc196e75ab87aa44dbb9dc90cbe2a223c1
Leandro Barros <leandroedbarros@gmail.com>
2022-05-07 08:47:49 -0300

    First commit

---
+++ src/test/java/com/southsystem/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southsystem.voteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
