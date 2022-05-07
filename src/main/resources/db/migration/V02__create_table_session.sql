b3c7bf91bf9dec3c0f09ed97af079b58b0dc7e1d
Leandro Barros <leandroedbarros@gmail.com>
2022-05-07 10:23:31 -0300

    Implementing database table migrations

---
+++ src/main/resources/db/migration/V02__create_table_session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE session (
+    id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    topic_id BIGINT(20),
+    startDate DATETIME NOT NULL,
+    endDate DATETIME NOT NULL,
+    open BOOLEAN NOT NULL,
+    FOREIGN KEY (topic_id) REFERENCES topic(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
