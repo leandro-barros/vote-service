6c2c2f11de2524a3130121b08a284b978916d23f
Leandro Barros <leandroedbarros@gmail.com>
2022-05-08 15:41:04 -0300

    Implementing external service call to check if member has voting permission

---
+++ src/main/java/com/southsystem/voteservice/dto/integration/DocumentDto.java
@@ -0,0 +1,12 @@
+package com.southsystem.voteservice.dto.integration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DocumentDto {
+
+    private String status;
+
+}
