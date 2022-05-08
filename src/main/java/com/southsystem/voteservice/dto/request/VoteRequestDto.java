4a5f8593643b5ec0b8c1a64b81dd888853226675
Leandro Barros <leandroedbarros@gmail.com>
2022-05-08 20:56:01 -0300

    Creating VoteController class API test

--- src/main/java/com/southsystem/voteservice/dto/request/VoteRequestDto.java
+++ src/main/java/com/southsystem/voteservice/dto/request/VoteRequestDto.java
@@ -1,11 +1,13 @@
 package com.southsystem.voteservice.dto.request;
 
 import com.southsystem.voteservice.model.Associate;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
+@Builder
 @Setter
 @Getter
 public class VoteRequestDto {
--- src/main/java/com/southsystem/voteservice/dto/response/VoteResultDto.java
+++ src/main/java/com/southsystem/voteservice/dto/response/VoteResultDto.java
@@ -1,10 +1,12 @@
 package com.southsystem.voteservice.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
 @Builder
 @Data
 public class VoteResultDto implements Serializable {
