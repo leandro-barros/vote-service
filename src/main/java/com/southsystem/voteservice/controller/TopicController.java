92b6294bf4c6c6f24fe26e0e70428f38516166fd
Leandro Barros <leandroedbarros@gmail.com>
2022-05-08 11:30:53 -0300

    Implementing API documentation with Swagger

--- src/main/java/com/southsystem/voteservice/controller/TopicController.java
+++ src/main/java/com/southsystem/voteservice/controller/TopicController.java
@@ -3,6 +3,7 @@ package com.southsystem.voteservice.controller;
 import com.southsystem.voteservice.service.TopicService;
 import com.southsystem.voteservice.dto.request.TopicRequestDto;
 import com.southsystem.voteservice.dto.response.TopicResponseDto;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,8 @@ public class TopicController {
         this.topicService = topicService;
     }
 
-    @PostMapping
+    @ApiOperation(value = "Save a topic")
+    @PostMapping(produces = "application/json;charset=UTF-8")
     public ResponseEntity<TopicResponseDto> save(@Valid @RequestBody TopicRequestDto topicRequestDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(topicService.save(topicRequestDto)) ;
     }
