da92818a609c63f8a697374d14cc77a151a5fd78
Leandro Barros <leandroedbarros@gmail.com>
2022-05-08 16:42:24 -0300

    Changing CPF heroku API URL in Apllication file

--- src/main/java/com/southsystem/voteservice/integration/DocumentClient.java
+++ src/main/java/com/southsystem/voteservice/integration/DocumentClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "email", url = "https://user-info.herokuapp.com/users")
+@FeignClient(value = "email", url = "${api.user-info}")
 public interface DocumentClient {
 
     @GetMapping("{cpf}")
