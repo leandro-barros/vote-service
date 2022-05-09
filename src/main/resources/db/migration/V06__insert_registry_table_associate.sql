9d0bfde480fcf2040b5785e319b56ee860073875
Leandro Barros <leandroedbarros@gmail.com>
2022-05-09 09:13:28 -0300

    Creating insert migration in associated and topic tables

---
+++ src/main/resources/db/migration/V06__insert_registry_table_associate.sql
@@ -0,0 +1,3 @@
+INSERT INTO associate(id, name, cpf) VALUES (NULL, 'Leandro', '70096852070');
+INSERT INTO associate(id, name, cpf) VALUES (NULL, 'Alexandre', '69249670001');
+INSERT INTO associate(id, name, cpf) VALUES (NULL, 'Alexandre', '27277564022');
